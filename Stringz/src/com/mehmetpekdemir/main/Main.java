8b1f9d0ff59818d0cf68c95d70b5137bfdc807c1
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-06-23 18:36:31 +0300

    Stringz

---
+++ Stringz/src/com/mehmetpekdemir/main/Main.java
@@ -0,0 +1,14 @@
+package com.mehmetpekdemir.main;
+
+import com.mehmetpekdemir.stringz.Stringz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(Stringz.countDuplicateCharacters("MEHMET"));
+		System.out.println(Stringz.firstNonRepeatedCharacter("MEHMET"));
+		System.out.println(Stringz.reverse("MEHMET"));
+		System.out.println(Stringz.containsOnlyDigits("123a"));
+	}
+
+}
